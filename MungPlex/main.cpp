3b392b89cbff9175362a30e871da3bce19528f33
LawnMeower <s_schmidt1993@outlook.de>
2023-02-28 18:06:13 +0100

    initial main code

--- MungPlex/main.cpp
+++ MungPlex/main.cpp
@@ -1,8 +1,115 @@
 ﻿#include <iostream>
+#include "MungPlexConfig.h"
+#include <stdio.h>
+#include "GLFW/glfw3.h"
+//#include "glad/glad.h"
+#include "imgui.h"
+#include "imgui_internal.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+#include "examples/libs/emscripten/emscripten_mainloop_stub.h"
+#include<string>
+#include "Connection.h"
+#include"Xertz.h"
 
-
-int main()
+namespace MungPlex
 {
-	std::cout << "Hello CMake." << std::endl;
+
+
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+
+
+int main(int argc, char* argv[])
+{
+	if (!glfwInit())
+		return EXIT_FAILURE;
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+	ImGui::StyleColorsDark();
+
+	GLFWwindow* window = glfwCreateWindow(720, 480, "MungPlex", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
+
+
+
+	
+
+	glfwSwapInterval(1);
+
+	ImGuiStyle& style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+
+
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	
+	//MungPlex::Connection::SystemInformations();
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
+
+		MungPlex::Connection::DrawWindow();
+
+
+		ImGui::Render();
+
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+		{
+			GLFWwindow* backup_current_context = glfwGetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			glfwMakeContextCurrent(backup_current_context);
+		}
+
+		glfwSwapBuffers(window);
+	}
+	
+
+	
+
+    
+	
+	
 	return 0;
 }
