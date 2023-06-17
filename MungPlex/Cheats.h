7b779c66d88984f1c6bd1cd4a790cb95dbd6e83a
LawnMeower <s_schmidt1993@outlook.de>
2023-06-17 22:17:04 +0200

    added ReadFromRAM function

--- MungPlex/Cheats.h
+++ MungPlex/Cheats.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "lua.hpp"
 #include "sol/sol.hpp"
 #include <cassert>
 #include "lua.h"
@@ -13,15 +14,25 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 #include "examples/libs/emscripten/emscripten_mainloop_stub.h"
+#include"Connection.h"
 
 namespace MungPlex
 {
-	static class Cheats
+    class Cheats
 	{
     private:
         Cheats()
         {
             _lua.open_libraries(sol::lib::base);
+            _lua.set("INT8", INT8);
+            _lua.set("INT16", INT16);
+            _lua.set("INT32", INT32);
+            _lua.set("INT64", INT64);
+            _lua.set("FLOAT", FLOAT);
+            _lua.set("DOUBLE", DOUBLE);
+            _lua.set("BOOL", BOOL);
+
+            _lua.set_function("ReadFromRAM", &readFromRAM);
         }
 
         ~Cheats()
@@ -49,6 +60,63 @@ namespace MungPlex
         void DrawCheatList(); //top-left
         void DrawCheatInformation(); //top-right
         void DrawControl(); //bottom left
+
+        static double readFromRAM(int type, uint64_t address)
+        {
+            bool bigEndian = Connection::IsBE();
+            int64_t readValue = 0;
+            double returnValue = 0.0;
+            int pid = Connection::GetCurrentPID();
+            std::vector<SystemRegion> regions = Connection::GetRegions();
+
+            for (int i = 0; i < regions.size(); ++i)
+            {
+                if (address < regions[i].Base || address >= regions[i].Base + regions[i].Size)
+                    continue;
+
+                void* readAddress = (char*)regions[i].BaseLocationProcess + address - regions[i].Base;
+
+                switch (type)
+                {
+                case INT8:
+                    Xertz::SystemInfo::GetProcessInfo(pid).ReadExRAM(&readValue, readAddress, 1);
+                    returnValue = (double)*(int8_t*)&readValue;
+                    break;
+                case BOOL:
+                    Xertz::SystemInfo::GetProcessInfo(pid).ReadExRAM(&readValue, readAddress, 1);
+                    returnValue = readValue != 0;
+                    break;
+                case INT16:
+                    Xertz::SystemInfo::GetProcessInfo(pid).ReadExRAM(&readValue, readAddress, 2);
+                    if (bigEndian) readValue = Xertz::SwapBytes<int16_t>(readValue);
+                    returnValue = (double)*(int16_t*)&readValue;
+                    break;
+                case INT32:
+                    Xertz::SystemInfo::GetProcessInfo(pid).ReadExRAM(&readValue, readAddress, 4);
+                    if (bigEndian) readValue = Xertz::SwapBytes<int32_t>(readValue);
+                    returnValue = (double)*(int32_t*)&readValue;
+                    break;
+                case INT64:
+                    Xertz::SystemInfo::GetProcessInfo(pid).ReadExRAM(&readValue, readAddress, 8);
+                    if (bigEndian) readValue = Xertz::SwapBytes<int64_t>(readValue);
+                    returnValue = (double)*(int64_t*)&readValue;
+                    break;
+                case FLOAT:
+                    Xertz::SystemInfo::GetProcessInfo(pid).ReadExRAM(&readValue, readAddress, 4);
+                    if (bigEndian) readValue = Xertz::SwapBytes<int32_t>(readValue);
+                    returnValue = (double)*(float*)&readValue;
+                    break;
+                default://DOUBLE
+                    Xertz::SystemInfo::GetProcessInfo(pid).ReadExRAM(&readValue, readAddress, 8);
+                    if (bigEndian) readValue = Xertz::SwapBytes<int64_t>(readValue);
+                    returnValue = *(double*)&readValue;
+                }
+
+                break;
+            }
+
+            return returnValue;
+        }
 
 
     public:
