969cc6f14225f3397d1585bd693ed84be641705d
Lawn Meower <s_schmidt1993@outlook.de>
2024-12-26 04:51:35 +0100

    WidgetHelpers: created ICombo interface class

---
+++ MungPlex/source/Helpers/WidgetHelpers/ICombo.cpp
@@ -0,0 +1,51 @@
+#include "ICombo.hpp"
+
+const uint64_t MungPlex::ICombo::GetCount() const
+{
+	return _stringPointers.size();
+}
+
+const uint64_t MungPlex::ICombo::GetSelectedIndex() const
+{
+	return _selectedIndex;
+}
+
+void MungPlex::ICombo::SetSelectedIndex(const uint64_t index)
+{
+	isInRange(index);
+	_selectedIndex = index;
+	callOnIndexChangedSlots();
+}
+
+const char* MungPlex::ICombo::GetCStringAt(const uint64_t index)
+{
+	isInRange(index);
+	return _stringPointers[index];
+}
+
+const char* MungPlex::ICombo::GetSelectedCString() const
+{
+	return _stringPointers[_selectedIndex];
+}
+
+const char* const* MungPlex::ICombo::GetCStringPointers() const
+{
+	return _stringPointers.data();
+}
+
+void MungPlex::ICombo::ConnectOnIndexChangedSlot(const Slot slot)
+{
+	_slotsOnIndexChanged.emplace_back(slot);
+}
+
+void MungPlex::ICombo::isInRange(const uint64_t index)
+{
+	if (index >= _stringPointers.size())
+		throw "Error: Index out of range!";
+}
+
+void MungPlex::ICombo::callOnIndexChangedSlots()
+{
+	for (Slot& slot : _slotsOnIndexChanged)
+		slot();
+}
---
+++ MungPlex/source/Helpers/WidgetHelpers/ICombo.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "IWidget.hpp"
+#include <utility>
+
+namespace MungPlex
+{
+    class ICombo : public IWidget
+    {
+    public:
+        ICombo(const std::string& label, const bool printLabel = true) : IWidget(label, printLabel) {}
+        virtual bool Draw(const float paneWidth = 0.25f, const float labelPortion = 0.4f) = 0;
+        const uint64_t GetCount() const;
+        const uint64_t GetSelectedIndex() const;
+        void SetSelectedIndex(const uint64_t index);
+        const char* GetCStringAt(const uint64_t index);
+        const char* GetSelectedCString() const;
+        const char* const* GetCStringPointers() const;
+        virtual const std::string& GetStdStringAt(const uint64_t index) = 0;
+        virtual const std::string& GetSelectedStdString() const = 0;
+        virtual uint32_t GetSelectedId() const = 0;
+        virtual void PopBack(const uint64_t count) = 0;
+        virtual void Clear() = 0;
+        void ConnectOnIndexChangedSlot(const Slot slot);
+
+    protected:
+        std::vector<const char*> _stringPointers{};
+        uint64_t _selectedIndex = 0;
+        std::vector<Slot> _slotsOnIndexChanged{};
+
+        void isInRange(const uint64_t index);
+        void callOnIndexChangedSlots();
+
+    private:
+    };
+}
